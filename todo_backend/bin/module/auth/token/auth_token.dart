3d2270dd095363d3f115348df820efaf15d2fc65
Pawan Kumar <pk89844@gmail.com>
2024-04-17 20:02:33 +0530

    Get task authorization added

--- todo_backend/bin/module/auth/token/auth_token.dart
+++ todo_backend/bin/module/auth/token/auth_token.dart
@@ -6,11 +6,16 @@
 // ignore_for_file: non_constant_identifier_names
 
 import 'dart:convert';
+import 'dart:typed_data';
 import 'package:cryptography/cryptography.dart';
 
 mixin AuthToken {
-  final String _SECRET_KEY = 'Honest';
-  Future<String> generateJWT(int userId, String fullname) async {
+  static Map<String, dynamic> _userData = {};
+
+  Map<String, dynamic> get jwtpayload => _userData;
+  static final String _SECRET_KEY = 'Honest';
+
+  static Future<String> generateJWT(int userId, String fullname) async {
     String algorithm = 'HS256';
     Map<String, dynamic> header = {'alg': algorithm, 'typ': 'JWT'};
     Map<String, dynamic> payload = {
@@ -36,5 +41,28 @@ mixin AuthToken {
     return jwt;
   }
 
-  void verifyJWT() {}
+  static Future<bool> verifyJWT(String token, String secretKey) async {
+    if (token.isNotEmpty && token != 'null') {
+      List<String> splitToken = token.split('.');
+      String encodedHeader = splitToken[0].trim();
+      String encodedPayload = splitToken[1].trim();
+      String encodedSignature = splitToken[2].trim();
+      Uint8List decodedHeader = base64Url.decode(encodedHeader);
+      Uint8List decodedPayload = base64Url.decode(encodedPayload);
+      // Uint8List decodedSignature = base64Url.decode(encodedSignature);
+      // String utf8Header = utf8.decode(decodedHeader);
+      String utf8Payload = utf8.decode(decodedPayload);
+      Map<String, dynamic> payload = json.decode(utf8Payload);
+      _userData = payload;
+      List<int> combinedUTF8 = decodedHeader + decodedPayload;
+      var hmac = Hmac.sha256();
+      var p = await hmac.calculateMac(combinedUTF8,
+          secretKey: SecretKey(secretKey.codeUnits));
+      var newP = await hmac.hashAlgorithm.hash(p.bytes);
+      var encode = base64Url.encode(newP.bytes);
+      return encode == encodedSignature;
+    } else {
+      return false;
+    }
+  }
 }
